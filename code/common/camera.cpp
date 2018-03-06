69035e33e7caacbedc2b774e29379d7853def5ef
Adam Peterson <apeterson@viztek.net>
2018-03-06 11:11:11 -0500

    Chapter 10

--- code/common/camera.cpp
+++ code/common/camera.cpp
@@ -1,6 +1,6 @@
 #include "camera.h"
 
-inline camera
+internal camera
 DefaultCamera()
 {
     camera Result;
@@ -8,6 +8,47 @@ DefaultCamera()
     Result.Horizontal = V3(4.0, 0.0, 0.0);
     Result.Vertical = V3(0.0, 2.0, 0.0);
     Result.Origin = V3(0.0, 0.0, 0.0);
+
+    return Result;
+}
+
+internal camera
+DefaultCameraWithFoV(r32 fovY, r32 Aspect)
+{
+    camera Result = {};
+
+    r32 Theta = ToRadians(fovY);
+    r32 HalfHeight = tan(Theta / 2.0);
+    r32 HalfWidth = Aspect*HalfHeight;
+
+    Result.LowerLeft = V3(-HalfWidth, -HalfHeight, -1.0);
+    Result.Horizontal = V3(HalfWidth*2.0, 0.0, 0.0);
+    Result.Vertical = V3(0.0, HalfHeight*2.0, 0.0);
+    Result.Origin = V3(0.0, 0.0, 0.0);
+
+    return Result;
+}
+
+internal camera
+MakeCamera(v3 *Eye, v3 *LookAt, v3 *Up, r32 fovY, r32 Aspect)
+{
+    camera Result;
+
+    v3 u, v, w;
+    r32 Theta = ToRadians(fovY);
+    r32 HalfHeight = tan(Theta / 2.0);
+    r32 HalfWidth = Aspect*HalfHeight;
+
+    Result.Origin = *Eye;
+    
+    w = Normalize(*Eye - *LookAt);
+    u = Normalize(Cross(*Up, w));
+    v = Cross(w, u);
+
+    Result.LowerLeft = V3(-HalfWidth, -HalfHeight, -1.0);
+    Result.LowerLeft = Result.Origin - HalfWidth*u - HalfHeight*v - w;
+    Result.Horizontal = 2.0*HalfWidth*u;
+    Result.Vertical = 2.0*HalfHeight*v;
 
     return Result;
 }
