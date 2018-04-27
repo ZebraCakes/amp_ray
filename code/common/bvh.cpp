9c5ba784910ab66a97c21fa1217f05f2d07a33ed
Adam Peterson <apeterson@viztek.net>
2018-04-27 16:16:30 -0400

    Left out some files

--- code/common/bvh.cpp
+++ code/common/bvh.cpp
@@ -1,7 +1,10 @@
 struct bvh_node
 {
-    bvh_node *Left;
-    bvh_node *Right;
+    void *Left;
+    void *Right;
+
+    hitable_type LeftType;
+    hitable_type RightType;
     rect3 AABB;
 };
 
@@ -40,6 +43,46 @@ HitNode(bvh_node *Node, ray *Ray, r32 tMin, r32 tMax, hit_record *Record)
     }
 
     return Result;
-
-
 }
+
+internal i32
+CompareXAxis()
+
+//TODO(Adam):  Accept more than just spheres.  How to do this in C?
+internal bvh_node
+ConstructBVH(hitable *root, i32 Count, r32 StartTime, r32 EndTime)
+{
+    bvh_node Result;
+    int SplitAxis = i32(3*RandomCanonical());
+    switch(SplitAxis)
+    {
+        case 0:
+        {
+            //TODO(Adam):  roll our own QuickSort for "fun"
+            qsort(&root, Count, sizeof(sphere), CompareXAxis);
+        }break;
+        case 1:
+        {
+            qsort(&root, Count, sizeof(sphere), CompareYAxis);
+        }break;
+        default:
+        {
+            qsort(&root, Count, sizeof(sphere), CompareZAxis);
+        }break;
+    }
+
+    if(Count == 1)
+    {
+        Result.Left = Result.Right = root;
+    }
+    else if(Count == 2)
+    {
+        Result.Left = root;
+        Result.Right = root + 1;
+    }
+    else
+    {
+        Result.Left = ()
+    }
+}
+
--- code/common/ray.h
+++ code/common/ray.h
@@ -60,6 +60,18 @@ union sphere
     };
 };
 
+enum hitable_type
+{
+    SPHERE,
+    PLANE,
+    BVH,
+};
+
+struct hitable
+{
+    hitable_type Type;
+    void *Data;
+}
 
 struct hit_record
 {
@@ -85,6 +97,128 @@ SphereCenterAtTime(sphere *Sphere, r32 CurrentTme)
     if(Sphere->State == MOVING)
     {
         Result = Sphere->StartPos + ((CurrentTme - Sphere->StartTime) / (Sphere->EndTime - Sphere->StartTime))*(Sphere->EndPos - Sphere->StartPos);
+    }
+
+    return Result;
+}
+
+// internal b32
+// RayBoxIntersection(rect3 *Rect, ray *Ray, r32 tMin, r32 tMax)
+// {
+//     b32 Result = true;
+
+//     r32 tx0 = ffmin((Rect->Min.x - Ray->Origin.x) / Ray->Direction.x), (Rect->Max.x - Ray->Origin.x) / Ray->Direction.x);
+//     r32 tx1 = ffmax((Rect->Min.x - Ray->Origin.x) / Ray->Direction.x), (Rect->Max.x - Ray->Origin.x) / Ray->Direction.x);
+
+//     r32 ty0 = ffmin((Rect->Min.y - Ray->Origin.y) / Ray->Direction.y), (Rect->Max.y - Ray->Origin.y) / Ray->Direction.y);
+//     r32 ty1 = ffmax((Rect->Min.y - Ray->Origin.y) / Ray->Direction.y), (Rect->Max.y - Ray->Origin.y) / Ray->Direction.y);
+
+//     r32 tz0 = ffmin((Rect->Min.z - Ray->Origin.z) / Ray->Direction.z), (Rect->Max.z - Ray->Origin.z) / Ray->Direction.z);
+//     r32 tz1 = ffmax((Rect->Min.z - Ray->Origin.z) / Ray->Direction.z), (Rect->Max.z - Ray->Origin.z) / Ray->Direction.z);
+
+//     r32 xMin = tMin = ffmax(tx0, tMin);
+//     r32 xMax = tMax = ffmin(tx1, tMax);
+
+//     r32 yMin = tMin = ffmax(ty0, tMin);
+//     r32 yMax = tMax = ffmin(ty1, tMax);
+
+//     r32 zMin = tMin = ffmax(tz0, tMin);
+//     r32 zMax = tMax = ffmin(tz1, tMax);
+    
+//     if (xMin > xMax || yMin > yMax || zMin > zMax)
+//     {
+//         Result = false;
+//     }
+
+//     return Result;
+// }
+
+inline b32
+FastRayBoxIntersection(rect3 *Rect, ray *Ray, r32 tMin, r32 tMax)
+{
+    b32 Result = true;
+    for(u32 Index = 0;
+        Index < 3;
+        ++Index)
+    {
+        r32 InverseDir = 1.0 / Ray->Direction.E[Index];
+        r32 t0 = (Rect->Min.E[Index] - Ray->Origin.E[Index])*InverseDir;
+        r32 t1 = (Rect->Max.E[Index] - Ray->Origin.E[Index])*InverseDir;
+
+        if(InverseDir < 0.0)
+        {
+            SWAP(t0, t1, r32);
+        }
+
+        tMin = (t0 > tMin) ? t0 : tMin;
+        tMax = (t1 > tMax) ? t1 : tMax;
+
+        if(tMax <= tMin)
+        {
+            Result = false;
+            break;
+        }
+    }
+
+    return Result;
+}
+
+inline b32
+AABB(sphere *Sphere, r32 t0, r32 t1, rect3 *Box)
+{
+    b32 Result = true;
+
+    *Box = Rectangle3(Sphere->Center - V3(Sphere->Radius, Sphere->Radius, Sphere->Radius), Sphere->Center + V3(Sphere->Radius, Sphere->Radius, Sphere->Radius));
+
+    return Result;
+}
+
+rect3
+AABB(rect3 *Box0, rect3 *Box1)
+{
+    rect3 Result;
+
+    v3 minBox = V3(fmin(Box0->Min.x, Box1->Min.x), 
+                   fmin(Box0->Min.y, Box1->Min.y),
+                   fmin(Box0->Min.z, Box1->Min.z));
+
+    v3 maxBox = V3(fmax(Box0->Max.x, Box1->Max.x), 
+                   fmax(Box0->Max.y, Box1->Max.y),
+                   fmax(Box0->Max.z, Box1->Max.z));
+
+    Result.Min = minBox;
+    Result.Max = maxBox;
+
+    return Result;
+}
+
+inline b32
+AABB(sphere *Spheres, u32 SphereCount, r32 t0, r32 t1, rect3 *Box)
+{
+    b32 Result = false;
+    if(SphereCount > 0)
+    {
+        rect3 TempBox;
+        Result = AABB(Spheres, t0, t1, &TempBox);
+
+        if(Result)
+        {
+            *Box = TempBox;
+            for(u32 Index = 1;
+                Index < SphereCount;
+                ++Index)
+            {
+                if(AABB(Spheres + Index, t0, t1, &TempBox))
+                {
+                    *Box = AABB(Box, &TempBox);
+                }
+                else
+                {
+                    Result = false;
+                    break;
+                }
+            }
+        }
     }
 
     return Result;
